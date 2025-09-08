1412f2e8fb98bbfeaeb4eeacc999f776b54639bf
jadmiko widodo <jadmiko@gmail.com>
2025-09-08 03:54:50 +0000

    studio-772216628:~/studio{main}$ npx cap init "AssetLoaderAds" "com.exam

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'AssetLoaderAds',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
